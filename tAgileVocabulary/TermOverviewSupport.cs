baadcbca3384e8a3424ddac616b5e034735ada5a
Alexey Glukhov <kanris2@gmail.com>
2017-06-02 15:52:27 +0300

    Move interaction logic from TermOverview to TermOverviewSupport

--- tAgileVocabulary/TermOverviewSupport.cs
+++ tAgileVocabulary/TermOverviewSupport.cs
@@ -1,12 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Windows;
+using System.Windows.Controls;
+using TermDBHandlerLibrary;
 namespace tAgileVocabulary
 {
-    public static class TermOverviewSupport
+    public class TermOverviewSupport
     {
+        private TermDBHandler db;
+        private Term editTerm;
+        private TermOverviewOperation operation; //Операция
+
+        public TermOverviewSupport(TermOverviewOperation operation, Term editTerm = null)
+        {
+            db = new TermDBHandler();
+
+            this.operation = operation;
+            this.editTerm = editTerm;
+        }
+
+        public void ChangeWindowType(Button btnRemoveTask, params TextBox[] textBoxes)
+        {
+            if (operation == TermOverviewOperation.ADD)
+            {
+                btnRemoveTask.Visibility = Visibility.Collapsed;
+
+            }
+            else
+            {
+                textBoxes[0].Text = editTerm.term;
+
+                textBoxes[1].Text = editTerm.transcription.Substring(1, editTerm.transcription.Length - 2);
+                textBoxes[2].Text = editTerm.interpretation;
+            }
+        }
+
+        public void SaveTerm(Window termOverview, params TextBox[] textBoxes)
+        {
+            if (textBoxes[0].Text.Length > 0)
+            {
+                if (operation == TermOverviewOperation.ADD)
+                {
+
+                    Term newTerm = createTerm(textBoxes);
+                    db.addTerm(newTerm);
+
+                }
+                else
+                {
+                    Term editedTerm = createTerm(textBoxes);
+                    editedTerm.termID = editTerm.termID;
+
+                    db.updateTerm(editedTerm);
+                }
+
+                termOverview.DialogResult = true;
+                termOverview.Close();
+
+            }
+            else
+            {
+                string message = Properties.Resources.TermFieldEmpty;
+                MessageBox.Show(message);
+            }
+        }
+
+        public void RemoveTerm(Window termOverview)
+        {
+            db.removeTerm(editTerm.termID);
+
+            termOverview.DialogResult = true;
+            termOverview.Close();
+        }
+
+        public void TranscriptionButtonPress(TextBox txTranscription, object sender)
+        {
+            string content = (sender as Button).Content.ToString();
+            txTranscription.Text += content;
+        }
+
+        private Term createTerm(params TextBox[] textBoxes)
+        {
+            string term = textBoxes[0].Text;
+            string transcription = textBoxes[1].Text;
+            string interpretation = textBoxes[2].Text;
+
+            return new Term() { term = term, transcription = transcription, interpretation = interpretation };
+        }
     }
 }
--- tAgileVocabulary/TermOverviewWindow.xaml.cs
+++ tAgileVocabulary/TermOverviewWindow.xaml.cs
@@ -9,85 +9,33 @@ namespace tAgileVocabulary
     /// </summary>
     public partial class TermOverviewWindow : Window
     {
-        TermDBHandler db = new TermDBHandler();
-
-        TermOverviewOperation operation; //Операция
-        Term editTerm;
+        private TermOverviewSupport tOverviewSupport;
 
         public TermOverviewWindow(TermOverviewOperation operation, Term editTerm)
         {
             InitializeComponent();
 
-            this.operation = operation;
-            this.editTerm = editTerm;
+            tOverviewSupport = new TermOverviewSupport(operation, editTerm);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (operation == TermOverviewOperation.ADD)
-            {
-                btnRemoveTask.Visibility = Visibility.Collapsed;
-
-            } else
-            {
-                txTerm.Text = editTerm.term;
-
-                txTranscription.Text = editTerm.transcription.Substring(1, editTerm.transcription.Length - 2);
-                txInterpretation.Text = editTerm.interpretation;
-            }
+            tOverviewSupport.ChangeWindowType(btnRemoveTask, txTerm, txTranscription, txInterpretation);
         }
 
         private void btnRemoveTask_Click(object sender, RoutedEventArgs e)
         {
-            db.removeTerm(editTerm.termID);
-
-            this.DialogResult = true;
-            this.Close();
+            tOverviewSupport.RemoveTerm(this);
         }
 
         private void btnSaveTask_Click(object sender, RoutedEventArgs e)
         {
-            if (txTerm.Text.Length > 0)
-            {
-                if (operation == TermOverviewOperation.ADD)
-                {
-
-                    Term newTerm = createTerm();
-                    db.addTerm(newTerm);
-
-                }
-                else
-                {
-                    Term editedTerm = createTerm();
-                    editedTerm.termID = editTerm.termID;
-
-                    db.updateTerm(editedTerm);
-                }
-
-                this.DialogResult = true;
-                this.Close();
-
-            } else
-            {
-                string message = Properties.Resources.TermFieldEmpty;
-                MessageBox.Show(message);
-            }
-            
-        }
-
-        private Term createTerm()
-        {
-            string term = txTerm.Text;
-            string transcription = txTranscription.Text;
-            string interpretation = txInterpretation.Text;
-
-            return new Term() { term = txTerm.Text, transcription = txTranscription.Text, interpretation = txInterpretation.Text };
+            tOverviewSupport.SaveTerm(this, txTerm, txTranscription, txInterpretation);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string content = (sender as Button).Content.ToString();
-            txTranscription.Text += content;
+            tOverviewSupport.TranscriptionButtonPress(txTranscription, sender);
         }
     }
 }
